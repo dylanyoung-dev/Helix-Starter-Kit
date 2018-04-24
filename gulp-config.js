516dbe4c1c19fbe3647e5ea12e6ac6ba111e221e
aiswebmaster <dylan@allisolutions.com>
2018-04-24 19:50:30 -0400

    Added Gulp + NPM changes

---
+++ gulp-config.js
@@ -0,0 +1,24 @@
+module.exports = function () {
+    var sitecoreRoot = "C:\\inetpub\\wwwroot\\starterkit.sc";
+    var config = {
+        websiteRoot: sitecoreRoot + "\\Website",
+        sitecoreLibraries: sitecoreRoot + "\\Website\\bin",
+        solutionName: "Helix",
+        licensePath: sitecoreRoot + "\\Data\\license.xml",
+        runCleanBuilds: false,
+        buildConfiguration: "Debug",
+        styles: {
+            source: [
+                `**/code/scss/**/*.{scss, sass, css}`,
+                `!**/code/css/**/*.{scss, sass, css}`
+            ],
+            build: sitecoreRoot + `\\Website\\css\\`
+        },
+        options: {
+            sass: {
+                outputStyle: 'compressed'
+            }
+        }
+    };
+    return config;
+};
---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require("gulp");
+var msbuild = require("gulp-msbuild");
+var debug = require("gulp-debug");
+var path = require("path");
+var nugetRestore = require("gulp-nuget-restore");
+var fs = require("fs");
+var util = require("gulp-util");
+var exec = require("child_process").exec;
+var merge = require("merge-stream");
+var runSequence = require("run-sequence");
+
+var $ = require('gulp-load-plugins')({ lazy: true });
+
+var config = require('./gulp-config.js')();
+
+module.exports.config = config;
+
+gulp.task('default', function (callback) {
+    return runSequence(
+        "Copy-Sitecore-Dlls",
+        callback);
+});
+
+////////////////////////////
+//    Move Sitecore Dlls
+////////////////////////////
+gulp.task('_Copy-Sitecore-Dlls', function () {
+
+    fs.statSync(config.sitecoreLibraries);
+
+    var files = config.sitecoreLibraries + "/**/*";
+
+    var libs = gulp.src(files).pipe(gulp.dest("./Libraries"));
+
+    return merge(libs);
+
+});
+
+////////////////////////////
+//    Publish All Projects
+////////////////////////////
+gulp.task('_Publish-All-Projects', function () {
+    var publishScript = `${__dirname}\\build\\Publish.ps1`;
+
+    var process = exec("powershell.exe -executionpolicy unrestricted -File \"" + publishScript + "\" -BuildConfiguration \"" + config.buildConfiguration + "\"", function (err, stdout, stderr) {
+        if (err !== null) throw err;
+        console.log(err);
+        console.log(stderr);
+        console.log(stdout);
+    });
+});
+
+////////////////////////////
+//     Compile Assets
+////////////////////////////
+gulp.task('_Compile-Assets', function () {
+    return runSequence("task:compile-styles", callback);
+});
+
+gulp.task('task:compile-styles', function () {
+
+    return gulp
+        .src(config.styles.source)
+        .pipe($.sass(config.options.sass))
+        .pipe($.flatten())
+        .pipe(gulp.dest(config.styles.build));
+
+});
