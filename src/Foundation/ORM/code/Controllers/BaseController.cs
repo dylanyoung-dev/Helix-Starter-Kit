9e417761c96ff5d2b23011a4c0ad1268e241c9fc
aiswebmaster <dylan@allisolutions.com>
2018-05-24 00:50:17 -0400

    Added Sitecore Dll's (Kernel + MVC)

---
+++ src/Foundation/ORM/code/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Helix.Foundation.ORM.Controllers
+{
+    public class BaseController
+    {
+    }
+}
