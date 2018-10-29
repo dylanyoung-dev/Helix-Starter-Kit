6a32304e36d84bef4a1aee2fcda8734f30f1128d
aiswebmaster <dylan@allisolutions.com>
2018-10-28 18:34:13 -0700

    Add to Solution on Project Project Add

--- generators/helix-project/index.js
+++ generators/helix-project/index.js
@@ -7,6 +7,7 @@ var mkdir = require('mkdirp');
 var guid = require('node-uuid');
 
 const prompts = require('../global/prompts/helix.project.prompts.js');
+const common = require('../global/common.js');
 
 module.exports = class extends Generator {
     constructor(args, opts) {
@@ -29,7 +30,7 @@ module.exports = class extends Generator {
     }
 
     configure() {
-        this.projectGuid = '{' + guid.v4() + '}';
+        this.projectGuid = guid.v4();
 
         this.targetPath = path.join('src', 'Project', this.projectName);
         
@@ -76,7 +77,7 @@ module.exports = class extends Generator {
         this.fs.copyTpl(
             this.templatePath('Project/code/.Sitecore.Project.csproj'),
             this.destinationPath(path.join(this.targetPath, 'code', this.solutionPrefix + '.Project.' + this.projectName + '.csproj')), {
-                projectGuid: this.projectGuid,
+                projectGuid: `{${this.projectGuid}}`,
                 projectName: this.projectName,
                 sitecoreVersion: '9.0.180604'
             }
@@ -118,6 +119,40 @@ module.exports = class extends Generator {
                 projectName: this.projectName
             }
         );
+    }
 
+    solutionAttach() {
+        let slnFilePath = common.getSolutionFilePath(this.destinationPath());
+
+        let slnText = this.fs.read(slnFilePath);
+        
+        slnText = common.ensureSolutionSection(slnText, 'ProjectConfigurationPlatforms', 'postSolution');
+        slnText = common.ensureSolutionSection(slnText, 'NestedProjects', 'preSolution');
+
+        let projectFolderGuid = guid.v4();
+
+        let projectDefinition =
+            `Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "${this.solutionPrefix}.Project.${this.projectName}", "src\\Project\\${this.projectName}\\code\\${this.solutionPrefix}.Project.${this.projectName}.csproj", "{${this.projectGuid}}"\r\n` +
+            `EndProject\r\n` +
+            `Project("{2150E333-8FDC-42A3-9474-1A3956D46DE8}") = "${this.projectName}", "${this.projectName}", "{${projectFolderGuid}}"\r\n` + `EndProject\r\n`;
+
+        let projectBuildConfig = 
+            `		{${this.projectGuid}}.Debug|Any CPU.ActiveCfg = Debug|Any CPU\r\n` +
+            `		{${this.projectGuid}}.Debug|Any CPU.Build.0 = Debug|Any CPU\r\n` +
+            `		{${this.projectGuid}}.Release|Any CPU.ActiveCfg = Release|Any CPU\r\n` +
+            `		{${this.projectGuid}}.Release|Any CPU.Build.0 = Release|Any CPU\r\n`;
+
+        slnText = common.ensureSolutionFolder(slnText, "Project");
+        let layerFolderGuid = common.getSolutionFolderGuid(slnText, "Project");
+
+        let projectNesting =
+            `		{${this.projectGuid}} = {${projectFolderGuid}}\r\n` +
+            `		{${projectFolderGuid}} = {${layerFolderGuid}}\r\n`;
+
+        slnText = slnText.replace(/\r\nMinimumVisualStudioVersion[^\r\n]*\r\n/, `$&${projectDefinition}\r\n`);
+        slnText = slnText.replace(/\r\n[^\r\n]*GlobalSection\(ProjectConfigurationPlatforms\)[^\r\n]*\r\n/, `$&${projectBuildConfig}\r\n`);
+        slnText = slnText.replace(/\r\n[^\r\n]*GlobalSection\(NestedProjects\)[^\r\n]*\r\n/, `$&${projectNesting}\r\n`);
+
+        this.fs.write(slnFilePath, slnText);
     }
 }
