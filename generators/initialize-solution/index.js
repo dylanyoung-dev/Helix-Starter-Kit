673e7cb6f07904c75806514f1fb1ae659cda2c86
aiswebmaster <dylan@allisolutions.com>
2018-10-28 22:07:19 -0700

    templating restructure for yeoman solution initialization

---
+++ generators/initialize-solution/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var Generator = require('yeoman-generator');
+var path = require('path');
+var chalk = require('chalk');
+var mkdir = require('mkdirp');
+var guid = require('node-uuid');
+
+const prompts = require('../global/prompts/solution.prompts.js');
+const common = require('../global/common.js');
