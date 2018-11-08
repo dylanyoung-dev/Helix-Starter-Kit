4863603e538f7d8ea5139857be2bb9c3c5e68a74
aiswebmaster <dylan@allisolutions.com>
2018-11-07 22:02:19 -0800

    Removed Scaffolding for Base Generator for Search + Common Project Folder

--- generators/initialize-solution/index.js
+++ generators/initialize-solution/index.js
@@ -85,9 +85,7 @@ module.exports = class extends Generator {
         let paths = [
             { Template: "/src/Foundation/Ioc/code/.Helix.Foundation.Ioc.csproj", Destination: "/src/Foundation/Ioc/code/", FileName: ".Foundation.Ioc" },
             { Template: "/src/Foundation/ORM/code/.Helix.Foundation.ORM.csproj", Destination: "/src/Foundation/ORM/code/", FileName: ".Foundation.ORM" },
-            { Template: "/src/Foundation/Search/code/.Helix.Foundation.Search.csproj", Destination: "/src/Foundation/Search/code/", FileName: ".Foundation.Search" },
-            { Template: "/src/Foundation/Serialization/code/.Helix.Foundation.Serialization.csproj", Destination: "/src/Foundation/Serialization/code/", FileName: ".Foundation.Serialization" },
-            { Template: "/src/Project/Common/code/.Helix.Project.Common.csproj", Destination: "/src/Project/Common/code/", FileName: ".Project.Common" }
+            { Template: "/src/Foundation/Serialization/code/.Helix.Foundation.Serialization.csproj", Destination: "/src/Foundation/Serialization/code/", FileName: ".Foundation.Serialization" }
         ];
 
         paths.forEach(function(path) {
@@ -108,9 +106,7 @@ module.exports = class extends Generator {
         let paths = [
             "\\src\\Foundation\\Ioc\\code\\",
             "\\src\\Foundation\\ORM\\code\\",
-            "\\src\\Foundation\\Search\\code\\",
             "\\src\\Foundation\\Serialization\\code\\",
-            "\\src\\Project\\Common\\code\\"
         ];
 
         paths.forEach(function(path) {
